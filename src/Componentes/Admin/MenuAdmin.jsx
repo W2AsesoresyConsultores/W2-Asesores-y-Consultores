5c325fb24d3ff416437c0faf247d231b10e9b182
Alexander0127 <AlexanderTR0127@gmail.com>
2024-11-05 18:01:40 -0500

    dark theme

--- src/Componentes/Admin/MenuAdmin.jsx
+++ src/Componentes/Admin/MenuAdmin.jsx
@@ -1,19 +1,19 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useState, useEffect } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
-import { FaSignOutAlt } from 'react-icons/fa';
+import { FaSignOutAlt, FaSun, FaMoon } from 'react-icons/fa';
 import { IoChatbubbleOutline, IoCalendarClearOutline, IoStatsChartOutline } from "react-icons/io5";
 import { RxAvatar, RxDashboard } from "react-icons/rx";
 import { useSpring, animated } from '@react-spring/web';
 import { supabase } from "../../supabase/supabase.config";
 
 // Componente reutilizable para los items del menú
-const MenuItem = ({ to, icon: Icon, label }) => {
+const MenuItem = ({ to, icon: Icon, label, theme }) => {
   const location = useLocation();
   const isActive = location.pathname === to;
 
   const styles = useSpring({
-    backgroundColor: isActive ? 'rgba(0, 123, 255, 0.2)' : 'transparent', // Azul claro cuando está activo
-    color: isActive ? '#007bff' : '#555', // Azul en activo, gris en inactivo
+    backgroundColor: isActive ? 'rgba(0, 123, 255, 0.2)' : 'transparent',
+    color: isActive ? '#007bff' : theme === 'light' ? '#555' : '#ccc',
   });
 
   return (
@@ -27,6 +27,15 @@ const MenuItem = ({ to, icon: Icon, label }) => {
 
 function MenuAdmin() {
   const navigate = useNavigate();
+  const [theme, setTheme] = useState(localStorage.getItem('menuTheme') || 'light');
+
+  useEffect(() => {
+    localStorage.setItem('menuTheme', theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme(prevTheme => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
 
   const handleLogout = async () => {
     try {
@@ -42,7 +51,6 @@ function MenuAdmin() {
     }
   };
 
-  // Memoriza el menú para evitar renders innecesarios
   const menuItems = useMemo(() => [
     { to: "/Admin", icon: RxDashboard, label: "Ofertas" },
     { to: "/Programa", icon: IoCalendarClearOutline, label: "Entrevistas" },
@@ -52,24 +60,27 @@ function MenuAdmin() {
   ], []);
 
   return (
-    <div className="w-64 h-screen z-10 font-lato bg-white text-gray-800 px-4 shadow-sm flex flex-col justify-between pt-8 fixed transition duration-500">
+    <div className={`w-60 h-screen z-10 font-lato ${theme === 'light' ? 'bg-white text-gray-800' : 'bg-gray-900 text-gray-300'} px-4 shadow-sm flex flex-col justify-between pt-8 fixed transition duration-500`}>
       {/* Header del menú */}
       <div className="flex justify-between items-center px-6 mb-8">
-        <h2 className="text-2xl font-semibold font-lato text-primarycolor">Panel Admin</h2>
+        <h2 className="text-2xl font-semibold font-dmsans text-primarycolor text-center">Power</h2>
+        <button onClick={toggleTheme} className="text-xl">
+          {theme === 'light' ? <FaMoon /> : <FaSun />}
+        </button>
       </div>
 
       {/* Menú de navegación */}
       <ul className="space-y-4">
         {menuItems.map(({ to, icon, label }) => (
           <li key={to}>
-            <MenuItem to={to} icon={icon} label={label} />
+            <MenuItem to={to} icon={icon} label={label} theme={theme} />
           </li>
         ))}
       </ul>
 
       {/* Botón de cerrar sesión */}
-      <div className="px-6 py-4">
-        <button onClick={handleLogout} className="flex items-center text-red-500 hover:text-red-600 transition duration-300 ease-in-out">
+      <div className="px-6 pb-12">
+        <button onClick={handleLogout} className="flex items-center text-red-500 transition duration-300 ease-in-out">
           <FaSignOutAlt className="mr-3 text-xl" /> Cerrar sesión
         </button>
       </div>
