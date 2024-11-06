5b7f979d770d2f261dda364db258aedb4ef75bb2
PieroNF <pgianmarco10@gmail.com>
2024-11-06 10:09:32 -0500

    Plantilla Excel de Candidatos

---
+++ src/Componentes/Entrevistas/DescargarPlantilla.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+import DownloadIcon from '@mui/icons-material/Download';
+
+function DescargarPlantilla() {
+  return (
+    <Button
+      variant="contained"
+      color="primary"
+      startIcon={<DownloadIcon />}
+      component="a"
+      href="/Plantilla.xlsx"
+      download="Plantilla.xlsx"
+    >
+      Descargar Plantilla
+    </Button>
+  );
+}
+
+export default DescargarPlantilla;
